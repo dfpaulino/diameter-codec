565a1f8c659cbd2b908ea60e781ad83f318ff0e4
dpaulino <dfpaulino@gmail.com>
2025-03-30 12:11:07 +0100

    add flowInformation AVP

--- src/test/java/org/example/diameter/avp/gx/ChargingRuleInstallTest.java
+++ src/test/java/org/example/diameter/avp/gx/ChargingRuleInstallTest.java
@@ -85,7 +85,11 @@ class ChargingRuleInstallTest {
 
     @Test
     public void chargingRuleInstall_ruleDefinition_encode() {
+
         //diameter.Session-Id == "string;459;844;IMSI999991234567810"
+        //diameter.Session-Id == "string;459;844;IMSI999991234567810"
+        byte[] bufferOrig = HexFormat.of().parseHex("000003e9c0000424000028af000003ebc000020c000028af000003edc000001f000028af5043433130342d514349392d44594e414d494300000001b74000000c00000034000001b04000000c0000000200000422c0000074000028af000001fbc0000046000028af7065726d697420696e2031372066726f6d203137322e31372e3234312e32353520313731303420746f203137322e31362e32302e3131312f33320000000003f6c000000e000028af68fc000000000438c0000010000028af0000000200000422c0000074000028af000001fbc0000047000028af7065726d6974206f75742031372066726f6d203137322e31362e32302e3131312f333220746f203137322e31372e3234312e32353520313731303400000003f6c000000e000028af68fc000000000438c0000010000028af00000001000001ffc0000010000028af00000002000003f8c0000078000028af00000404c0000010000028af0000000900000204c0000010000028af00003e8000000203c0000010000028af00002fa80000040ac000003c000028af00000416c0000010000028af0000000f00000417c0000010000028af0000000100000418c0000010000028af00000001000003efc0000010000028af00000001000003f2c0000010000028af00000001000003f1c0000010000028af00000001000003f0c0000010000028af00000000000001b2400000180000043e80000010000028af00000000000003ebc000020c000028af000003edc000001f000028af5043433130352d514349392d44594e414d494300000001b74000000c00000035000001b04000000c0000000300000422c0000074000028af000001fbc0000046000028af7065726d697420696e2031372066726f6d203137322e31372e3234312e32353520313731303520746f203137322e31362e32302e3131312f33320000000003f6c000000e000028af68fc000000000438c0000010000028af0000000200000422c0000074000028af000001fbc0000047000028af7065726d6974206f75742031372066726f6d203137322e31362e32302e3131312f333220746f203137322e31372e3234312e32353520313731303500000003f6c000000e000028af68fc000000000438c0000010000028af00000001000001ffc0000010000028af00000002000003f8c0000078000028af00000404c0000010000028af0000000900000204c0000010000028af00003e8000000203c0000010000028af00002fa80000040ac000003c000028af00000416c0000010000028af0000000f00000417c0000010000028af0000000100000418c0000010000028af00000001000003efc0000010000028af00000001000003f2c0000010000028af00000001000003f1c0000010000028af00000001000003f0c0000010000028af00000000000001b2400000180000043e80000010000028af00000000");
+
         ChargingRuleInstall cri = new ChargingRuleInstall();
 
         ChargingRuleDefinition crd1 = new ChargingRuleDefinition();
@@ -107,8 +111,21 @@ class ChargingRuleInstallTest {
         qos1.setAllocationRetentionPriority(arp);
         crd1.setQoSInformation(qos1);
 
+        FlowInformation flowInformation1 = new FlowInformation();
+        flowInformation1.setFlowDescription(new FlowDescription("permit in 17 from 172.17.241.255 17104 to 172.16.20.111/32"));
+        flowInformation1.setFlowDirection(new FlowDirection(2));
+        flowInformation1.setTosTrafficClass(new TosTrafficClass(HexFormat.of().parseHex("68fc")));
+
+        FlowInformation flowInformation2 = new FlowInformation();
+        flowInformation2.setFlowDescription(new FlowDescription("permit out 17 from 172.16.20.111/32 to 172.17.241.255 17104"));
+        flowInformation2.setFlowDirection(new FlowDirection(1));
+        flowInformation2.setTosTrafficClass(new TosTrafficClass(HexFormat.of().parseHex("68fc")));
+
+        crd1.setFlowInformation(flowInformation1);
+        crd1.setFlowInformation(flowInformation2);
         cri.setChargingRuleDefinition(crd1);
 
+        /*#################*/
         ChargingRuleDefinition crd2 = new ChargingRuleDefinition();
         crd2.setChargingRuleName(new ChargingRuleName("PCC105-QCI9-DYNAMIC"));
         crd2.setServiceIdentifier(new ServiceIdentifier(53));
@@ -128,12 +145,26 @@ class ChargingRuleInstallTest {
         qos2.setAllocationRetentionPriority(arp);
         crd2.setQoSInformation(qos2);
 
+
+        FlowInformation flowInformation11 = new FlowInformation();
+        flowInformation11.setFlowDescription(new FlowDescription("permit in 17 from 172.17.241.255 17105 to 172.16.20.111/32"));
+        flowInformation11.setFlowDirection(new FlowDirection(2));
+        flowInformation11.setTosTrafficClass(new TosTrafficClass(HexFormat.of().parseHex("68fc")));
+
+        FlowInformation flowInformation21 = new FlowInformation();
+        flowInformation21.setFlowDescription(new FlowDescription("permit out 17 from 172.16.20.111/32 to 172.17.241.255 17105"));
+        flowInformation21.setFlowDirection(new FlowDirection(1));
+        flowInformation21.setTosTrafficClass(new TosTrafficClass(HexFormat.of().parseHex("68fc")));
+
+        crd2.setFlowInformation(flowInformation11);
+        crd2.setFlowInformation(flowInformation21);
         cri.setChargingRuleDefinition(crd2);
 
         byte[] encoded =cri.encode();
 
-        assertEncoded(encoded,cri);
 
+        assertEncoded(encoded,cri);
+        //assertThat(encoded).isEqualTo(bufferOrig);
     }
 
     private void assertEncoded(byte[] actual,ChargingRuleInstall criExpected) {
@@ -174,6 +205,9 @@ class ChargingRuleInstallTest {
             assertThat(qoSInformation.getAllocationRetentionPriority().getData().getPreEmptionCapability().getData())
                     .isEqualTo(criExpected.getChargingRuleDefinition().get(i).getQoSInformation().getAllocationRetentionPriority().getPreEmptionCapability().getData());
 
+            assertThat(cri.getData().getChargingRuleDefinition().getFirst().getData().getFlowInformation().size()).isEqualTo(2);
+            assertThat(cri.getData().getChargingRuleDefinition().getFirst().getData().getFlowInformation().getFirst().getData().getFlowDescription().getData())
+                    .isEqualTo(criExpected.getData().getChargingRuleDefinition().getFirst().getData().getFlowInformation().getFirst().getFlowDescription().getData());
 
 
         }
